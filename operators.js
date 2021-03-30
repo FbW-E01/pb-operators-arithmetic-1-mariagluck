039188f54c0edeb5f8ab7fa1f1e443919a697c5e
mariagluck <mariamart.berlin@gmail.com>
2021-03-30 13:49:06 +0200

    maths in JS

---
+++ operators.js
@@ -0,0 +1,54 @@
+let snackServings = 20;
+
+let guests = 10;
+
+console.log(snackServings);
+console.log(guests);
+
+
+console.log(snackServings + guests);
+
+
+
+console.log(snackServings - guests);
+
+
+
+console.log(snackServings * guests);
+
+
+console.log(snackServings / guests);
+
+
+const drinks = 10;
+console.log(drinks);
+
+const resultOne = (snackServings * guests) / drinks;
+console.log(resultOne);
+
+// 5. Declare two variables: "monkeys" with value 15, and "bananas" with value 9. Divide monkeys by bananas.
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys, bananas);
+
+console.log(monkeys / bananas);
+
+// 6. Declare another variable "monkeyfights" with value 20. Add monkeys and bananas, then multiply the result by monkeyfights. Store the result in variable "resultTwo".  Print "resultTwo" to the console.
+let monkeyfights = 20;
+const resultTwo = (monkeys + bananas) * monkeyfights;
+console.log(monkeyfights);
+
+// 7. Increment monkeys.
+monkeyfights++;
+console.log(monkeyfights);
+
+// 8. Decrement bananas.
+bananas--;
+console.log(bananas);
+
+// 9. Subtract monkeys from bananas and store this in a new variable "happiness". Add happiness and monkeyfights.
+
+const happiness = monkeys + bananas;
+console.log(monkeys + happiness);
+// 10. Print the remainder when "resultO
+console.log(resultOne % resultTwo);
